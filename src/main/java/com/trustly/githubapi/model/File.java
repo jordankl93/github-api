c4509498c12dd876873e744eb9c50ed94506998c
jordankl93 <jordankl93@gmail.com>
2020-03-15 01:25:16 -0300

    Mudança para o Heroku

--- src/main/java/com/trustly/githubapi/model/File.java
+++ src/main/java/com/trustly/githubapi/model/File.java
@@ -6,9 +6,10 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,7 +27,8 @@ public class File implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy="increment")
 	private Long id;
 	
 	@Column(name = "name")
